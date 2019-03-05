ff52effaf3da412b2f163dffbc592f0ca6fd4f4d
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-03-05 20:50:08 +0300

    add .action

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import getDifference from '..';
 
 program
   .version('1.0.1')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
+  .action((fileBefore, fileAfter) => {
+    console.log(getDifference(fileBefore, fileAfter));
+  })
   .parse(process.argv);
