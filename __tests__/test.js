f3be515c48fe1885ee84ad6f0b2eee565ec68264
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-03-06 09:44:07 +0300

    add tests for .yaml

--- __tests__/test.js
+++ __tests__/test.js
@@ -16,3 +16,14 @@ test('diff json 2', () => {
   const expected = fs.readFileSync(getPath('expected1.txt'), 'utf8');
   expect(gendiff(getPath('before1.json'), getPath('after1.json'))).toBe(expected);
 });
+
+test('diff yaml 1', () => {
+  const expected = fs.readFileSync(getPath('expected.txt'), 'utf8');
+  expect(gendiff(getPath('before.yaml'), getPath('after.yaml'))).toBe(expected);
+});
+
+
+test('diff yaml 2', () => {
+  const expected = fs.readFileSync(getPath('expected1.txt'), 'utf8');
+  expect(gendiff(getPath('before1.yaml'), getPath('after1.yaml'))).toBe(expected);
+});
