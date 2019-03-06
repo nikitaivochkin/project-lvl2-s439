9ceac6d8a2b905a70be23bb0d6f72b241d356665
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-03-06 13:51:41 +0300

    refactoring parsers

--- src/parsers.js
+++ src/parsers.js
@@ -2,18 +2,13 @@ import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const parsersAction = [
-  {
-    check: pathToFile => (path.extname(pathToFile) === '.json'),
-    parser: JSON.parse,
-  },
-  {
-    check: pathToFile => (path.extname(pathToFile) === '.yaml'),
-    parser: yaml.safeLoad,
-  },
-  {
-    check: pathToFile => (path.extname(pathToFile) === '.ini'),
-    parser: ini.parse,
-  },
-];
-export default pathToFile => parsersAction.find(({ check }) => check(pathToFile)).parser;
+const parsersAction = {
+  '.json': JSON.parse,
+  '.yaml': yaml.safeLoad,
+  '.ini': ini.parse,
+};
+
+export default (pathToFile) => {
+  const expansion = path.extname(pathToFile);
+  return parsersAction[expansion];
+};
