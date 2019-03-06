d390d4f62f6af8a7fd7adbd4d969922e37310f47
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-03-06 10:52:32 +0300

    test.each instead test

--- __tests__/test.js
+++ __tests__/test.js
@@ -4,26 +4,18 @@ import fs from 'fs';
 import path from 'path';
 import gendiff from '../src';
 
-const getPath = fileName => path.join('__tests__', '__fixtures__', `${fileName}`);
-
-test('diff json 1', () => {
-  const expected = fs.readFileSync(getPath('expected.txt'), 'utf8');
-  expect(gendiff(getPath('before.json'), getPath('after.json'))).toBe(expected);
-});
-
-
-test('diff json 2', () => {
-  const expected = fs.readFileSync(getPath('expected1.txt'), 'utf8');
-  expect(gendiff(getPath('before1.json'), getPath('after1.json'))).toBe(expected);
-});
-
-test('diff yaml 1', () => {
-  const expected = fs.readFileSync(getPath('expected.txt'), 'utf8');
-  expect(gendiff(getPath('before.yaml'), getPath('after.yaml'))).toBe(expected);
-});
-
-
-test('diff yaml 2', () => {
-  const expected = fs.readFileSync(getPath('expected1.txt'), 'utf8');
-  expect(gendiff(getPath('before1.yaml'), getPath('after1.yaml'))).toBe(expected);
-});
+test.each([
+  ['before.json', 'after.json', 'expected.txt'],
+  ['before1.json', 'after1.json', 'expected1.txt'],
+  ['before.yaml', 'after.yaml', 'expected.txt'],
+  ['before1.yaml', 'after1.yaml', 'expected1.txt'],
+  ['before.ini', 'after.ini', 'expected.txt'],
+  ['before1.ini', 'after1.ini', 'expected1.txt'],
+])(
+  '.gendiff(%#)',
+  (before, after, expectedFile) => {
+    const getPath = fileName => path.join('__tests__', '__fixtures__', `${fileName}`);
+    const expected = fs.readFileSync(getPath(expectedFile), 'utf8');
+    expect(gendiff(getPath(before), getPath(after))).toBe(expected);
+  },
+);
