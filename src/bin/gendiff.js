4d9348dae68135fc85191e36c74ec56a19274b34
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-03-06 11:06:59 +0300

    v.1.0.6

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import program from 'commander';
 import getDifference from '..';
 
 program
-  .version('1.0.1')
+  .version('1.0.6')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
