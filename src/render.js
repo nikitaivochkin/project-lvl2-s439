f035d339c9c005990bfcba1e00662aa6cbd563fb
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-03-07 20:38:14 +0300

    change render

--- src/render.js
+++ src/render.js
@@ -14,15 +14,29 @@ const stringify = (value, level) => {
   return `{\n  ${getIndent(level + 1)} ${_.flatten(values).join('\n')}\n${getIndent(level + 1)}}`;
 };
 
+const actions = [
+  {
+    typeNode: 'unchanged',
+    action: (key, value, indentLevel) => ` ${getIndent(indentLevel)} ${' '} ${key}: ${stringify(value, indentLevel)}`,
+  },
+  {
+    typeNode: 'changed',
+    action: (key, value, indentLevel) => ` ${getIndent(indentLevel)} ${'-'} ${key}: ${stringify(value[0], indentLevel)}\n ${getIndent(indentLevel)} ${'+'} ${key}: ${stringify(value[1], indentLevel)}`,
+  },
+  {
+    typeNode: 'added',
+    action: (key, value, indentLevel) => ` ${getIndent(indentLevel)} ${'+'} ${key}: ${stringify(value, indentLevel)}`,
+  },
+  {
+    typeNode: 'deleted',
+    action: (key, value, indentLevel) => ` ${getIndent(indentLevel)} ${'-'} ${key}: ${stringify(value, indentLevel)}`,
+  },
+];
+
 const renderNode = (node, indentLevel) => {
   const { type, key, value } = node;
-  if (type === 'unchanged') {
-    return ` ${getIndent(indentLevel)} ${' '} ${key}: ${stringify(value, indentLevel)}`;
-  } if (type === 'changed') {
-    return ` ${getIndent(indentLevel)} ${'-'} ${key}: ${stringify(value[0], indentLevel)}\n ${getIndent(indentLevel)} ${'+'} ${key}: ${stringify(value[1], indentLevel)}`;
-  } if (type === 'added') {
-    return ` ${getIndent(indentLevel)} ${'+'} ${key}: ${stringify(value, indentLevel)}`;
-  } return ` ${getIndent(indentLevel)} ${'-'} ${key}: ${stringify(value, indentLevel)}`;
+  const { action } = actions.find(({ typeNode }) => typeNode === type);
+  return action(key, value, indentLevel);
 };
 
 export default (data) => {
