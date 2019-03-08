7765c4a82cd56119abd1e0ef8fc5ecb809d155e3
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-03-08 16:14:16 +0300

    fix mentor's edit

--- src/renderers/renderSimpleFormat.js
+++ src/renderers/renderSimpleFormat.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import { car, cdr } from 'hexlet-pairs';
 
 const getIndent = (level) => {
   const deepIndent = 4;
@@ -15,33 +14,19 @@ const stringify = (value, level) => {
   return `{\n  ${getIndent(level + 1)} ${_.flatten(values).join('\n')}\n${getIndent(level + 1)}}`;
 };
 
-const actions = [
-  {
-    typeNode: 'parent',
-    action: (key, value, indentLevel, f) => `   ${getIndent(indentLevel)} ${key}: {\n${f(value, indentLevel + 1)}\n    ${getIndent(indentLevel)}}`,
-  },
-  {
-    typeNode: 'unchanged',
-    action: (key, value, indentLevel) => ` ${getIndent(indentLevel)} ${' '} ${key}: ${stringify(value, indentLevel)}`,
-  },
-  {
-    typeNode: 'changed',
-    action: (key, value, indentLevel) => ` ${getIndent(indentLevel)} ${'-'} ${key}: ${stringify(car(value), indentLevel)}\n ${getIndent(indentLevel)} ${'+'} ${key}: ${stringify(cdr(value), indentLevel)}`,
-  },
-  {
-    typeNode: 'added',
-    action: (key, value, indentLevel) => ` ${getIndent(indentLevel)} ${'+'} ${key}: ${stringify(value, indentLevel)}`,
-  },
-  {
-    typeNode: 'deleted',
-    action: (key, value, indentLevel) => ` ${getIndent(indentLevel)} ${'-'} ${key}: ${stringify(value, indentLevel)}`,
-  },
-];
+const actions = {
+  parent: (key, _value, children, indentLevel, f) => `   ${getIndent(indentLevel)} ${key}: {\n${f(children, indentLevel + 1)}\n    ${getIndent(indentLevel)}}`,
+  unchanged: (key, value, _children, indentLevel) => ` ${getIndent(indentLevel)} ${' '} ${key}: ${stringify(value, indentLevel)}`,
+  changed: (key, value, _children, indentLevel) => ` ${getIndent(indentLevel)} ${'-'} ${key}: ${stringify(value.keyBefore, indentLevel)}\n ${getIndent(indentLevel)} ${'+'} ${key}: ${stringify(value.keyAfter, indentLevel)}`,
+  added: (key, value, _children, indentLevel) => ` ${getIndent(indentLevel)} ${'+'} ${key}: ${stringify(value, indentLevel)}`,
+  deleted: (key, value, _children, indentLevel) => ` ${getIndent(indentLevel)} ${'-'} ${key}: ${stringify(value, indentLevel)}`,
+};
 
 const renderNode = (node, indentLevel, renderAst) => {
-  const { type, key, value } = node;
-  const { action } = actions.find(({ typeNode }) => typeNode === type);
-  return action(key, value, indentLevel, renderAst);
+  const {
+    type, key, value, children,
+  } = node;
+  return actions[type](key, value, children, indentLevel, renderAst);
 };
 
 export default (data) => {
