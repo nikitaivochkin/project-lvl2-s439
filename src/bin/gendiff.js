d37696cb8ee9c457fc8eaef3606627fcf0d16161
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-03-08 11:22:16 +0300

    add option format

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,11 +4,11 @@ import program from 'commander';
 import getDifference from '..';
 
 program
-  .version('1.0.6')
+  .version('1.1.0')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
-  .action((fileBefore, fileAfter) => {
-    console.log(getDifference(fileBefore, fileAfter));
+  .action((fileBefore, fileAfter, option) => {
+    console.log(getDifference(fileBefore, fileAfter, option.format));
   })
   .parse(process.argv);
