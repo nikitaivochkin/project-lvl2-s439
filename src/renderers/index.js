e5c640e5d52c566d7eb579ab5f99fc80a805c28e
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-03-08 16:38:02 +0300

    rename

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,9 +1,11 @@
 import renderSimpleFormat from './renderSimpleFormat';
 import renderPlainFormat from './renderPlainFormat';
+import renderJsonFormat from './renderJsonFormat';
 
 const renderFormats = {
   simple: renderSimpleFormat,
   plain: renderPlainFormat,
+  json: renderJsonFormat,
 };
 
 export default (ast, format) => renderFormats[format](ast);
