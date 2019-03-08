603e929a48d0ee563b250c7f36879d64bbbe3966
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-03-08 16:38:11 +0300

    rename

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import findParser from './parsers';
-import renderAst from './renderers/plain';
+import renderAst from './renderers';
 
 const actions = [
   {
